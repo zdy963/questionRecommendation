c12a162abd2f95418efd5a38ac233693151320fb
Zhang Dongyu <zhangdongyu963@outlook.com>
2018-03-18 21:48:20 +0800

    增加了题目和选项写入数据库的函数

--- questionProcessing/write_db.py
+++ questionProcessing/write_db.py
@@ -3,11 +3,19 @@ import time
 
 cur_time = time.strftime("%Y-%m-%d", time.localtime())
 
-def write_que_bank(qid):
-    sql = """INSERT INTO SJD_QUESTION_BANK VALUES ()"""
+def write_que_bank(qid, cursor):
+    sql = """INSERT INTO SJD_QUESTION_BANK (ID, CREATE_TIME) VALUES (%d, %s)""" % (qid, cur_time)
+    cursor.execute(sql)
 
 
-def write_question():
+def write_question(qid,cursor,question):
+    sql = "INSERT INTO SJD_MULTI_CHOICE (QID, QUESTION) VALUES (%d, %s)"% (qid,question)
+    cursor.execute(sql)
 
 
-def write_choices()
+def write_choices(qid,cursor,ans_num, ans):
+    sql = "INSERT INTO SJD_MULTI_CHOICE(SELECTION_%s) VALUES (%s) WHERE QID = %d" % (ans_num, ans,qid)
+    cursor.execute(sql)
+
+
+def write_ans(qid,cursor,ans):
