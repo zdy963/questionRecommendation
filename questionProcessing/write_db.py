ec1223b26fef75a264c60554ad2bc92a9e8b863e
Zhang Dongyu <zhangdongyu963@outlook.com>
2018-03-21 11:05:49 +0800

    增加了题目字典，使传入数据库时可以更简洁

--- questionProcessing/write_db.py
+++ questionProcessing/write_db.py
@@ -1,21 +1,14 @@
-import pymysql
+# -*- coding: utf-8 -*-
 import time
 
-cur_time = time.strftime("%Y-%m-%d", time.localtime())
 
 def write_que_bank(qid, cursor):
-    sql = """INSERT INTO SJD_QUESTION_BANK (ID, CREATE_TIME) VALUES (%d, %s)""" % (qid, cur_time)
+    cur_time = "%s" % time.strftime("%Y-%m-%d", time.localtime())
+    sql = "INSERT INTO SJD_QUESTION_BANK (ID, CREATE_TIME) VALUES (%d, str_to_date(%s,'%%Y-%%m-%%d'))" % (qid, cur_time)
     cursor.execute(sql)
 
 
-def write_question(qid,cursor,question):
-    sql = "INSERT INTO SJD_MULTI_CHOICE (QID, QUESTION) VALUES (%d, %s)"% (qid,question)
+def write_multi_choice(qid, que_dict, cho_dict, cursor):
+    sql = "INSERT INTO SJD_MULTI_CHOICE VALUES (%d, '%s', '%s', '%s', '%s', '%s',NULL,NULL,%d)"\
+          % (qid,que_dict[qid][0],cho_dict[qid][0],cho_dict[qid][1],cho_dict[qid][2],cho_dict[qid][3], que_dict[qid][1])
     cursor.execute(sql)
-
-
-def write_choices(qid,cursor,ans_num, ans):
-    sql = "INSERT INTO SJD_MULTI_CHOICE(SELECTION_%s) VALUES (%s) WHERE QID = %d" % (ans_num, ans,qid)
-    cursor.execute(sql)
-
-
-def write_ans(qid,cursor,ans):
